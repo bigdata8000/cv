e31c18165365e2dcd068bc2cd0b0cc93807d2208
bigdata8000 <bigdata8000@gmail.com>
2021-04-06 16:53:56 +0100

    New updates for Heroku deployment

--- resume/settings.py
+++ resume/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'by#i1a&8t!e(#_@+eiz6h=oa@+ns_-@f4)n#imicz##=0f+lsw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
