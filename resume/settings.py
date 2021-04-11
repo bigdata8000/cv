c0d88d3308e4a459d91d9637a2d9bff9470380b8
bigdata8000 <bigdata8000@gmail.com>
2021-04-11 12:18:25 +0100

    first commit again

--- resume/settings.py
+++ resume/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,3 +118,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# Activate Django-Heroku
+django_heroku.settings(locals())
